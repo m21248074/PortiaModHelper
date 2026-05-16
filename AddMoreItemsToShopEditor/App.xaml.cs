114677de03c1e5f19473c5cdba22f1480c3535bf
Cavey <m.123051427@gmail.com>
2026-05-16 21:58:03 +0800

    加入專案檔案。

---
+++ AddMoreItemsToShopEditor/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace AddMoreItemsToShopEditor
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
