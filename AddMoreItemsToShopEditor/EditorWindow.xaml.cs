f3614fb5db96bb63fe311aae81f7682c76151838
Cavey <m.123051427@gmail.com>
2026-05-16 23:35:20 +0800

    feat: 更新編輯器視窗，新增 JSON 路徑顯示及加載功能，並引入 ShopItem 模型

--- AddMoreItemsToShopEditor/EditorWindow.xaml.cs
+++ AddMoreItemsToShopEditor/EditorWindow.xaml.cs
@@ -1,4 +1,7 @@
-﻿using System.Windows;
+﻿using AddMoreItemsToShopEditor.Models;
+using System.IO;
+using System.Text.Json;
+using System.Windows;
 
 namespace AddMoreItemsToShopEditor
 {
@@ -9,7 +12,9 @@ namespace AddMoreItemsToShopEditor
     {
         private string _gamePath;
         private string _jsonPath;
-        
+
+        private List<ShopItem>? _shopItemsList = new List<ShopItem>();
+
         public EditorWindow(string gamePath, string jsonPath)
         {
             InitializeComponent();
@@ -17,8 +22,36 @@ namespace AddMoreItemsToShopEditor
             _gamePath = gamePath;
             _jsonPath = jsonPath;
 
-            TxtPassedGamePath.Text = _gamePath;
             TxtPassedJsonPath.Text = _jsonPath;
+
+            this.Loaded += EditorWindow_Loaded;
+        }
+
+        private async void EditorWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                if (File.Exists(_jsonPath))
+                {
+                    string jsonContent = await File.ReadAllTextAsync(_jsonPath);
+
+                    _shopItemsList = JsonSerializer.Deserialize<List<ShopItem>>(jsonContent);
+
+                    if (_shopItemsList != null && _shopItemsList.Count > 0)
+                    {
+                        DgModItems.ItemsSource = _shopItemsList;
+                    }
+                    else
+                    {
+                        MessageBox.Show("The JSON file is empty or contains no items.", "Warning", MessageBoxButton.OK, MessageBoxImage.Warning);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Failed to load JSON data in Editor Workspace:\n{ex.Message}",
+                                "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
         }
     }
 }
---
+++ AddMoreItemsToShopEditor/Models/ShopItem.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AddMoreItemsToShopEditor.Models
+{
+    class ShopItem
+    {
+        public int ID { get; set; }
+        public int count { get; set; }
+        public int currency { get; set; }
+        public int requireMission { get; set; }
+        public int storeId { get; set; }
+        public double chance { get; set; }
+    }
+}
