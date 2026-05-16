dd3e5a92d0eeeb78ab4cb6f6ff54bd09c5cb87b0
Cavey <m.123051427@gmail.com>
2026-05-17 00:42:56 +0800

    feat: 新增 GameItem 和 GameStore 類別，並更新 ShopItem 類別以移除不必要的 using 語句

---
+++ AddMoreItemsToShopEditor/Models/GameItem.cs
@@ -0,0 +1,13 @@
+﻿namespace AddMoreItemsToShopEditor.Models
+{
+    class GameItem
+    {
+        public int ID { get; set; }
+        public string EnglishName { get; set; }
+        public string TargetLangName { get; set; }
+        public string Description { get; set; }
+        public int BuyPrice { get; set; }
+        public int SellPrice { get; set; }
+        public int StackNumber { get; set; }
+    }
+}
---
+++ AddMoreItemsToShopEditor/Models/GameStore.cs
@@ -0,0 +1,10 @@
+﻿namespace AddMoreItemsToShopEditor.Models
+{
+    class GameStore
+    {
+        public int StoreId { get; set; }
+        public string EnglishName { get; set; }
+        public string TargetLangName { get; set; }
+        public string OpenTime { get; set; }
+    }
+}
